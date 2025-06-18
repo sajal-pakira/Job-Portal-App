380079ce432f94e87734c45fd189ba712fd62ed9
Sajal Pakira <uniquesajal6@gmail.com>
2025-06-18 21:59:40 +0530

    job schema added

--- models/job.model.js
+++ models/job.model.js
@@ -1,7 +1,46 @@
 import mongoose from "mongoose";
 
-const jobSchema = mongoose.Schema({
-    
-}, { timestamps: true });
+const jobSchema = mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Company name is required"],
+    },
+    position: {
+      type: String,
+      required: [true, "Job position is required"],
+      minlength: 100,
+    },
+    position: {
+      type: String,
+      required: [true, "Job position is required"],
+    },
+    status: {
+      type: String,
+      enum: ["pending", "reject", "interview"],
+      default: "pending",
+    },
+    workType: {
+      type: String,
+      enum: ["full-time", "part-time", "internship", "freelance"],
+      default: "full-time",
+    },
+    jobType: {
+      type: String,
+      enum: ["Remote", "On-site", "Hybrid"],
+      default: "On-site",
+    },
+    workLocation: {
+      type: String,
+      required: [true, "work location is required"],
+      default: "Bengaluru, India",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Job", jobSchema);
