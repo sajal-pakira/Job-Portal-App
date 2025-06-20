dac281d67c77b44ad9e7df52cb125795a4e2920e
Sajal Pakira <uniquesajal6@gmail.com>
2025-06-20 16:02:08 +0530

    improved error message

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,15 +3,20 @@ import jwt from "jsonwebtoken";
 const userAuth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return next(new Error("Authentication failed"));
+    return next(
+      new Error(
+        "Authentication failed, Header is missing or it doesn't start with Bearer "
+      )
+    );
   }
+
   const token = authHeader.split(" ")[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     req.user = { userId: payload.userId };
     next();
   } catch (error) {
-    return next(new Error("Auth failed"));
+    return next(new Error("Authentication failed"));
   }
 };
 
