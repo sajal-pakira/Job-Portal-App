1d2944a3039132a3d3ef7bf886023e66cf63e635
Sajal Pakira <uniquesajal6@gmail.com>
2025-06-21 17:51:10 +0530

    security fixed

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,24 @@
 import express from "express";
-import { loginController, registerController } from "../controllers/auth.controller.js";
+import {
+  loginController,
+  registerController,
+} from "../controllers/auth.controller.js";
+import rateLimit from "express-rate-limit";
 
 const router = express.Router();
 
+// IP limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
 //register post
-router.post("/register", registerController);
+router.post("/register", limiter, registerController);
 //login post
-router.post("/login",loginController );
+router.post("/login", limiter, loginController);
 
 export default router;
--- server.js
+++ server.js
@@ -6,11 +6,15 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import "express-async-errors";
+// security packages
+import helmet from "helmet";
+import xss from "xss";
+import ExpressMongoSanitize from "express-mongo-sanitize";
 //files import
 import connectDB from "./config/db.js";
 import authRoute from "./routes/auth.route.js";
-import userRoute from './routes/user.route.js'
-import jobRoute from './routes/job.route.js'
+import userRoute from "./routes/user.route.js";
+import jobRoute from "./routes/job.route.js";
 
 //mongoDb connection
 connectDB();
@@ -22,11 +26,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+app.use(helmet(``));
+app.use(xss(``));
+app.use(ExpressMongoSanitize())
 
 //routes
 app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/job",jobRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/job", jobRoute);
 
 //validation middleware
 app.use(errorMiddleware);
